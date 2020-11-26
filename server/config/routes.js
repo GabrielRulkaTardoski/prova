bc135ea75565f7c1aef709da69fd323d1a7bc62f
Gabriel Rulka Tardoski <gabrieltardoski@hotmail.com>
2020-11-26 19:53:48 -0300

    Tive que instalar uma versão antiga porquê a nova de node NÃO SUPORTA W7
    AAAAAAAAAAAAAA

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,7 +4,7 @@ const pessoaController = require("../controllers/PessoaController.js");
 /*router.get("/", usuarioController.Get);
 */
 router.post("/Pessoa/Cadastrar", pessoaController.Cadastrar);
-router.post("/Pessoa/Listar", pessoaController.Listar);
+router.get("/Pessoa/Listar", pessoaController.Listar);
 router.post("/Pessoa/Buscar/:cpfPessoa", pessoaController.Buscar);
 router.post("/Pessoa/Alterar", pessoaController.Alterar);
 router.get("/Pessoa/Remover/:cpfPessoa", pessoaController.Remover);
