2685fbe5cfefbee00eddb7e3fe492ab592359d96
Gabriel Rulka Tardoski <gabrieltardoski@hotmail.com>
2020-11-26 22:19:30 -0300

    aaa

--- client/src/app/models/pessoa.ts
+++
@@ -1,7 +0,0 @@
-export class pessoa
-{
-    _id?:string;
-    nome:string; 
-    cpf:string;
-    idade:number
-}
--- client/src/app/services/pessoa.service.ts
+++ client/src/app/services/pessoa.service.ts
@@ -5,9 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class PessoaService 
 {
+<<<<<<< HEAD
   constructor(private http: HttpClient ) { }
   BASEURL = "http://localhost:1234/";
   cadastrar(pessoa: pessoa): Observable<pessoa>{
     return this.http.post<pessoa>(this.BASEURL+"pessoa/cadastrar",pessoa);
   }
+=======
+  constructor() { }
+>>>>>>> parent of 39d1636... Cadastrar
 }
