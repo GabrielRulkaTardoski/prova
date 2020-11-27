39d16362c6c09e1a2fe45f8989e01359d6f37e09
Gabriel Rulka Tardoski <gabrieltardoski@hotmail.com>
2020-11-26 22:05:48 -0300

    Cadastrar

--- client/src/app/services/pessoa.service.ts
+++ client/src/app/services/pessoa.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { pessoa } from '../models/pessoa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PessoaService 
 {
-  constructor() { }
+
+  constructor(private http: HttpClient ) { }
+  BASEURL = "http://localhost:1234/";
+  cadastrar(pessoa: pessoa): Observable<pessoa>{
+    return this.http.post<pessoa>(this.BASEURL+"medico/cadastrar",pessoa);
+  }
 }
