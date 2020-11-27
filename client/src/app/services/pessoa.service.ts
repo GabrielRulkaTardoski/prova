b4192a4c19c28d1c64dd5644a6f7ea79c87b92a4
Gabriel Rulka Tardoski <gabrieltardoski@hotmail.com>
2020-11-26 22:18:27 -0300

    Update pessoa.service.ts

--- client/src/app/services/pessoa.service.ts
+++ client/src/app/services/pessoa.service.ts
@@ -5,13 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class PessoaService 
 {
-<<<<<<< HEAD
   constructor(private http: HttpClient ) { }
   BASEURL = "http://localhost:1234/";
   cadastrar(pessoa: pessoa): Observable<pessoa>{
     return this.http.post<pessoa>(this.BASEURL+"pessoa/cadastrar",pessoa);
   }
-=======
-  constructor() { }
->>>>>>> parent of 39d1636... Cadastrar
 }
